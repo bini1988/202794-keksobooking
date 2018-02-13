a7dfd87c8b26a0ccfd7058593d8a47a898c899fc
bini1988 <rus.nick@mail.ru>
2018-02-13 18:16:30 +0300

    Code refactoring

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,21 +1,13 @@
-const commands = [
-  require(`./author`),
-  require(`./description`),
-  require(`./license`),
-  require(`./version`),
-];
-
-module.exports = {
+module.exports = (commands = []) => ({
   name: `help`,
   description: `печатает этот текст`,
   execute() {
     const NAME_FIELD_WIDTH = 15;
 
     console.log(`Доступные команды:`);
-    console.log(`${this.name.padEnd(NAME_FIELD_WIDTH)} - ${this.description};`);
 
     for (const {name, description} of commands) {
       console.log(`${name.padEnd(NAME_FIELD_WIDTH)} - ${description};`);
     }
   }
-};
+});
